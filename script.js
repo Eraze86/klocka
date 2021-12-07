3814dcdffd8555dd5e4f4361188d33fe1474f9f5
Eraze86 <therese.rosvall@hotmail.se>
2021-12-07 20:40:03 +0100

    fixade så extra nollor syntes i tiden

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const clock = document.getElementById("clock");
 
 setInterval(function () {
 
-    let hour = new Date();
-    let min = new Date();
-    let sec = new Date();
+    let hour = new Date().getHours();
+    let min = new Date().getMinutes();
+    let sec = new Date().getSeconds();
 
-    clock.innerHTML = hour.getHours() + ":" + min.getMinutes() + ":" + sec.getSeconds();
+
+    
 
     if(hour < 10){
         hour = "0"+hour;
-        console.log(hour)
     }
 
     if(min < 10){
@@ -18,8 +18,9 @@ setInterval(function () {
     }
   
     if(sec < 10){
-        sec = "0"+sec;
-        
+        sec = "0"+sec; 
     }
+
+    clock.innerHTML = hour + ":" + min + ":" + sec ;
 
 }, 1000);
